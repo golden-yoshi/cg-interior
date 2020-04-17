b8ede61980510b06055978d2ae7ec5d0bfa467c9
golden-yoshi <gunnykins@gmail.com>
2020-04-17 18:51:11 +1000

    Fixed lighting issue. Changed floor materials to Lambert. Tidied JS files.

---
+++ RoomLighting.js
@@ -0,0 +1,18 @@
+//lighting - basic light from camera towards the scene
+var cameralight = new THREE.PointLight(new THREE.Color(1, 1, 1), 0.5);
+camera.add(cameralight);
+scene.add(camera);
+
+//ambient lighting
+var ambientlight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.2);
+scene.add(ambientlight);
+
+var spotlight = new THREE.SpotLight(new THREE.Color(1, 1, 1), 0.5);
+spotlight.position.x = 10;
+spotlight.position.z = 90;
+spotlight.angle = Math.PI / 8;
+spotlight.penumbra = 0.3;
+spotlight.castShadow = true;
+scene.add(spotlight);
+var spotLightHelper = new THREE.SpotLightHelper(spotlight);
+scene.add(spotLightHelper);
